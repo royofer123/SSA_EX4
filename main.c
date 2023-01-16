e86a771c73d71b3154359300e37b9be3082779e8
Kfir Zilbernagel <98819583+Kfirul@users.noreply.github.com>
2023-01-16 22:42:54 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <limits.h>
+#include <stdbool.h>
 
 pnode findNode(pnode *head,int num){
     node *curr = *head;
@@ -188,18 +189,6 @@ void delete_node_cmd(node** head, int data)
 }
 }
 
-int graphSize(pnode head){
-    if (head == NULL)
-        return 0;
-
-    pnode curr = head;
-    int count=0;
-    while (curr != NULL ) {
-        curr = curr->next;
-        count++;
-    }
-return count;
-}
 
 
 
@@ -214,166 +203,36 @@ void deleteGraph_cmd(pnode* head){
     }
 }
 
-typedef struct queueNode_{
-    int node_num;
-    int dis;
-    struct queueNode_ *next; 
-}qNode, *pqNode;
-
-pqNode createQNode(int num,int currDis){
-    pqNode newNode=(pqNode)malloc(sizeof(qNode));
-    newNode -> node_num=num;
-    newNode -> dis= currDis;
-    newNode -> next = NULL;
-    return newNode;
-}
-
-void insertQNode(pqNode *headQ1,pqNode newNode){
-    if((*headQ1)==NULL)
-        *headQ1= newNode;
-
-    if((*headQ1) -> node_num > newNode -> node_num){
-        newNode -> next =*headQ1;
-        *headQ1= newNode;
-    }
-
-    pqNode curr = *headQ1;
-    while(curr ->next != NULL){
-        if(curr ->next ->node_num > newNode -> node_num){
-            newNode -> next = curr ->next;
-            curr -> next = newNode;
-            return;
-        }
-    }
-    curr->next = newNode;
-}
-
-pqNode pollQNode(pqNode *head){
-    pqNode curr= *head;
-    *head= curr -> next;
-    return curr;
-
-}
-
-int isEmpty(pqNode head){
-	if( head == NULL);
-        return 1;
-    return 0;
-}
-
-int findIndex(int arr[],int size,int name){
-    for(int i=0;i<size;i++){
-        if(arr[i]==name)
-        return i;
-    }
-    return -1;
-}
-
 int findMax(pnode *head){
     int max=-1;
     pnode curr=*head;
     while(curr!= NULL){
         if(curr->node_num>max)
         max= curr ->node_num;
+        curr=curr ->next;
     }
     return max;
 }
 
-int dijkstra(pnode *head,int source,int desitination){
-    if (head == NULL)
-        return -1;
 
-
-    // int sizeOfGraph=graphSize(*head);
-     //int names[sizeOfGraph];
-    // int dist[sizeOfGraph];
-    // int visited[sizeOfGraph];
-    int max =findMax(head) +1;
-
-    int *dist = (int *)malloc(sizeof(int) * max);
-    int *visited = (int *)malloc(sizeof(int) * max);
-   // int * names = (int *)malloc(sizeof(int) * max);
-    pnode curr= *head;
-    // int i=0;
-    // while(curr!=NULL){
-    //     names[i]=curr -> node_num;
-    //     curr=curr ->next;
-    //     i++;
-    // }
-
-    pqNode *headQ=(pqNode)malloc(sizeof(pqNode));
-    pqNode sourceNode;
-    for(int i=0;i<max;i++){
-        if(i==source){
-            dist[i]=0;
-            sourceNode= createQNode(i,0);
-            insertQNode(headQ,sourceNode);
-            visited[i]=1;
-        }
-
-        else dist[i]=INT_MAX;    
-    } 
-
-    for (int i = 0; i < max; i++){
-        visited[i] = 0;
-        if (findNode(head, i) == NULL)
-            visited[i] = 1;
-    }
-
-     
-    pedge currEdge;
-    //int index;
-       
-
-    while(isEmpty(*headQ)==0){
-       pqNode now=pollQNode(headQ);
-        curr=findNode(head,now->node_num);
-        currEdge=curr -> edges;
-        while(currEdge!=NULL){
-            if(visited[curr->node_num]!=1){
-                if(now->dis + currEdge ->weight < dist[curr->node_num]){
-                    dist[curr->node_num]=now->dis + currEdge ->weight;
-                    insertQNode(headQ,createQNode(currEdge ->endpoint ->node_num,dist[curr->node_num]));
-                }
-                visited[curr->node_num]=1;
-            } 
-        }
-        
-        free(now);
-    }
-
-    int disFar =dist[desitination];
-    free(*headQ);
-    free(dist);
-    //free(names);
-    free(visited);
-
-    if(disFar==INT_MAX)
-        return -1;
-
-    return disFar;
-}
 int shortest_path(pnode* head, int src, int dest){
-    int node_max_size = 0;
-    pnode tmp = *head;
-    while(tmp){
-        if(node_max_size < tmp -> node_num){
-            node_max_size = tmp -> node_num;
-        }
-        tmp = tmp -> next;
-    }
-    int dist[node_max_size+1];
-    int visited [node_max_size+1];
+    int max = findMax(head);
+    
+    int dist[max+1];
+    int visited [max+1];
     int i;
-    for(i = 0; i < node_max_size+1; i++){
+    for(i = 0; i < max+1; i++){
         dist[i] = (INT_MAX);
         visited[i] = -1;
     }
-    tmp = *head;
+
+    pnode tmp = *head;
+
     while (tmp){
         visited[tmp->node_num] = 1;
         tmp=tmp->next;
     }
+
     dist[src]=0;
     while(1){
         int min_node_id;
@@ -392,7 +251,7 @@ int shortest_path(pnode* head, int src, int dest){
         int current_value;
         int new_value;
         tmp = *head;
-        while(tmp->node_num!=min_node_id){
+        while(tmp->node_num != min_node_id){
             tmp=tmp->next;
         }
         pedge tmp_edge=tmp->edges;
@@ -411,62 +270,188 @@ int shortest_path(pnode* head, int src, int dest){
     }
     return dist[dest];
 }
-int TSP(pnode *head){
-    int size = 0;
-    scanf("%d", &size);
-    int nodes[size];
-    int j;
-    for (j = 0; j < size; j++) {
-        scanf("%d", &nodes[j]);
+
+// int TSP(pnode *head){
+//     int size = 0;
+//     scanf("%d", &size);
+//     int nodes[size];
+//     int j;
+//     for (j = 0; j < size; j++) {
+//         scanf("%d", &nodes[j]);
+//     }
+//     int min_dist=INT_MAX;
+//     pnode tmp = *head;
+//     // pnode point_to_list = *head;
+//     int i;
+//     for(i=0; i<size;i++){
+//         int current=0;
+//         int src_id = nodes[i];
+//         int nodes_temp[size];
+//         int j;
+//         for(j=0;j<size;j++){
+//             nodes_temp[j]=nodes[j];
+//         }
+//         nodes_temp[i]=-1;
+//         while(1){
+//             int ans;
+//             int current_dest;
+//             int min_current=INT_MAX;
+//             for(j=0;j<size;j++) {
+//                 if (nodes_temp[j] != -1) {
+//                     tmp = *head;
+//                     ans = shortest_path(&tmp, src_id,nodes_temp[j]);
+//                     if(ans<min_current && ans!=-1 && ans>0){
+//                         min_current=ans;
+//                         current_dest=j;
+//                     }
+//                     if(ans<0 || ans == -1){
+//                         min_current=INT_MAX;
+//                         current=INT_MAX;
+//                         break;
+//                     }
+//                 }
+//             }
+//             if(min_current==INT_MAX){
+//                 break;
+//             }
+//             current+=min_current;
+//             nodes_temp[current_dest]=-1;
+//             src_id=nodes[current_dest];
+//         }
+//         if(current<min_dist){
+//             min_dist=current;
+//         }
+//     }
+//     if(min_dist==INT_MAX){
+//         return -1;
+//     }
+//     return min_dist;
+// }
+// void TSP_helper_cmd(pnode head, int *arr,int num, int curr,int *pmin)
+// {
+//     if(num==2)
+//     {
+//         int finshdist = shortest_path(head,arr[0],arr[1]);
+//         if(finshdist!=INT_MAX)
+//         {
+//             if((curr + finshdist)<*pmin)
+//             {
+//                 *pmin = (curr+finshdist);
+//             }
+//         }
+//         return;
+//     }
+//     for(int i=1;i<num;i++)
+//     {
+//         swap_place(arr,1,i);
+//         int dist = shortest_path(head,arr[0],arr[1]);
+//         if(dist==INT_MAX)
+//         {
+//             return;
+//         }
+//         TSP_helper_cmd(head,arr+1,num-1,curr+dist,pmin);
+//         swap_place(arr,i,1);
+//     }
+// }
+
+// void swap_place(int *arr, int num1, int num2)
+// {
+//     int temp = arr[num1];
+//     arr[num1]=arr[num2];
+//     arr[num2]=temp;
+// }
+// int TSP_cmd(pnode head)
+// {
+//     int num;
+//     scanf("%d", &num);
+//     int arr = (int)(malloc(sizeof(int)*num));
+//     int min = INT_MAX;
+//     int * pmin=&min;
+//     for(int i =0 ; i<num; i++)
+//     {
+//         scanf("%d", &arr[i]);
+//     }
+//     for(int i=0;i<num;i++)
+//     {
+//         swap_place(arr,0,i);
+//         TSP_helper_cmd(head,arr,num,0,pmin);
+//         swap_place(arr,i,0);
+//     }
+//     free(arr);
+//     return *pmin;
+// }
+int min(int num1, int num2) {
+    return (num1 >= num2) ? num2 : num1;
+}
+void swap(int *first, int *second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+
+void reverse(int *permutation, int start_index, int size) {
+    int i = start_index;
+    int j = size - 1;
+    while (i < j) {
+        swap(&permutation[i], &permutation[j]);
+        i++;
+        j--;
     }
-    int min_dist=INT_MAX;
-    pnode tmp = *head;
-    // pnode point_to_list = *head;
-    int i;
-    for(i=0; i<size;i++){
-        int current=0;
-        int src_id = nodes[i];
-        int nodes_temp[size];
-        int j;
-        for(j=0;j<size;j++){
-            nodes_temp[j]=nodes[j];
-        }
-        nodes_temp[i]=-1;
-        while(1){
-            int ans;
-            int current_dest;
-            int min_current=INT_MAX;
-            for(j=0;j<size;j++) {
-                if (nodes_temp[j] != -1) {
-                    tmp = *head;
-                    ans = shortest_path(&tmp, src_id,nodes_temp[j]);
-                    if(ans<min_current && ans!=-1 && ans>0){
-                        min_current=ans;
-                        current_dest=j;
-                    }
-                    if(ans<0 || ans == -1){
-                        min_current=INT_MAX;
-                        current=INT_MAX;
-                        break;
-                    }
-                }
-            }
-            if(min_current==INT_MAX){
+}
+
+
+int compare(const void *a, const void *b) {
+    return (*(int *) a - *(int *) b);
+}
+
+bool next_permutation(int *permutation, int size) {
+    int i = size - 2;
+    while (i >= 0 && permutation[i] >= permutation[i + 1]) {
+        i--;
+    }
+    if (i == -1) {
+        return false;
+    }
+    int j = size - 1;
+    while (permutation[j] <= permutation[i]) {
+        j--;
+    }
+    swap(&permutation[i], &permutation[j]);
+    reverse(permutation, i + 1, size);
+    return true;
+}
+void TSP_cmd(pnode head) {
+    int size, dijskAns;
+    scanf("%d", &size);
+//    int *permutation=(int*) malloc(sizeof(int)*size);
+    int permutation[size];
+    for (int i = 0; i < size; i++) {
+        scanf("%d", &permutation[i]);
+    }
+    qsort(permutation, size, sizeof(int), compare);
+    int min_path = INT_MAX;
+
+    do {
+        int current_path_weight = 0;
+        for (int i = 0; i < size-1; i++) {
+            dijskAns = shortest_path(&head, permutation[i], permutation[i + 1]);
+            if (dijskAns == INT_MAX) {
+                current_path_weight = INT_MAX;
                 break;
             }
-            current+=min_current;
-            nodes_temp[current_dest]=-1;
-            src_id=nodes[current_dest];
+            current_path_weight += dijskAns;
+
         }
-        if(current<min_dist){
-            min_dist=current;
-        }
-    }
-    if(min_dist==INT_MAX){
-        return -1;
-    }
-    return min_dist;
+        min_path = min(min_path, current_path_weight);
+
+    } while (next_permutation(permutation, size));
+
+    if (min_path != INT_MAX) {
+        printf("TSP shortest path: %d \n", min_path);
+    } else printf("TSP shortest path: -1 \n");
 }
+
 int main(){
     
     char choice;
@@ -496,10 +481,19 @@ int main(){
             int answer =shortest_path(head,ver1,ver2);
             printf("Dijsktra shortest path: %d \n",answer);
        }
-       if(choice=='T'){
-        int answer =TSP(head);
-        printf("TSP shortest path: %d \n",answer);
-            }
+       if(choice == 'T')
+        {
+            // int ans = TSP_cmd(head);
+            // if(ans ==INT_MAX)
+            // {
+            //     printf("TSP shortest path: -1 \n");
+            // }
+            // else
+            // {
+            //     printf("TSP shortest path: %d \n",ans);
+            // }
+            TSP_cmd(*head);
+        }
        
     //    if(choice=='W')
     //    break;
